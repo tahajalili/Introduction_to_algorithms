aadce428b67e763c7ea17185e846a227a6758b8a
Taha Jalili <tahajalili@gmail.com>
2022-11-30 14:26:37 -0500

    Initial Commit

---
+++ insertionSort/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  insertionSort
+//
+//  Created by Taha Jalili on 30.11.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
